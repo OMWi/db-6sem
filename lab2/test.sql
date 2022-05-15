079d198dca6c6497a7fcde44ff4e7185e6b67f5c
OMWi <bsuiromwi@gmail.com>
2022-05-15 22:35:08 +0300

    lab2 upd

--- lab2/test.sql
+++ lab2/test.sql
@@ -1,12 +1,11 @@
+insert into groups(name, c_val) values('group1', 0);
+insert into groups(name, c_val) values('group1', 0);
+insert into groups(name, c_val) values('group2', 0);
+
 insert into students(name, group_id) values('name1', 1);
 insert into students(name, group_id) values('name2', 2);
 insert into students(id, name, group_id) values(2, 'name3', 1);
 insert into students(id, name, group_id) values(3, 'name3', 1);
 update students set name='omwi' where name='name1';
-insert into groups(name, c_val) values('group1', 20);
-insert into groups(name, c_val) values('group1', 25);
-insert into groups(name, c_val) values('group2', 30);
-delete from groups where id = 1;
-truncate table groups;
-truncate table students;
-truncate table logs_students;
+
+--delete from groups where id = 1;
